6c7a4c10ef9a4e2382dda0e0398e850466f28400
Germano Caberlan Smaniotto <germsmaniotto@gmail.com>
2023-12-09 07:17:50 -0300

    Added solution to day 9

--- Day_09.py
+++ Day_09.py
@@ -8,16 +8,44 @@ def open_file(file_name: str = "Day_09.txt") -> str:
         return f.read()
 
 
-def part_one(lines: list[str]) -> int:
-    pass
+def reduce_line(line: list[int]) -> list[int]:
+    result = []
+    for i in range(len(line) - 1):
+        result.append(line[i + 1] - line[i])
+    return result
 
 
-def part_two(lines: list[str]) -> int:
-    pass
+def part_one(lines: list[list[int]]) -> int:
+    result = 0
+    for line in lines:
+        derivatives_matrix = [line]
+        while not all(derivative == 0 for derivative in derivatives_matrix[-1]):
+            derivatives_matrix.append(reduce_line(derivatives_matrix[-1]))
+
+        result += sum(derivatives[-1] for derivatives in derivatives_matrix)
+    return result
+
+
+def part_two(lines: list[list[int]]) -> int:
+    result = 0
+    for line in lines:
+        derivatives_matrix = [line]
+        while not all(derivative == 0 for derivative in derivatives_matrix[-1]):
+            derivatives_matrix.append(reduce_line(derivatives_matrix[-1]))
+
+        first_derivatives = [derivatives[0] for derivatives in derivatives_matrix]
+        zeroth_column = [0] * len(first_derivatives)
+
+        for index in range(len(zeroth_column) - 2, -1, -1):
+            zeroth_column[index] = first_derivatives[index] - zeroth_column[index + 1]
+
+        result += zeroth_column[0]
+    return result
 
 
 def main():
     lines = open_file().splitlines()
+    lines = [list(map(int, line.split())) for line in lines]
     print("Part 1: ", part_one(lines))
     print("Part 2: ", part_two(lines))
 
